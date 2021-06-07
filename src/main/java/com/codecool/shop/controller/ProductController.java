b4ffc9d0baa29d74787e158b3dd32de668009114
tothgeza <toth.geza.0425@gmail.com>
2021-06-07 15:12:08 +0200

    Modify Products by Category

--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -2,8 +2,10 @@ package com.codecool.shop.controller;
 
 import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.SupplierDao;
 import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
 import com.codecool.shop.dao.implementation.ProductDaoMem;
+import com.codecool.shop.dao.implementation.SupplierDaoMem;
 import com.codecool.shop.model.ProductCategory;
 import com.codecool.shop.service.ProductService;
 import com.codecool.shop.config.TemplateEngineUtil;
@@ -25,9 +27,10 @@ public class ProductController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String page = req.getParameter("category");
-        System.out.println(page);
+        String supplier = req.getParameter("supplier");
         ProductDao productDataStore = ProductDaoMem.getInstance();
         ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
+        SupplierDao supplierDao = SupplierDaoMem.getInstance();
         ProductService productService = new ProductService(productDataStore,productCategoryDataStore);
 
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
@@ -35,7 +38,6 @@ public class ProductController extends HttpServlet {
         if (page != null) {
             for (ProductCategory pd : productCategoryDataStore.getAll()) {
                 if (page.equals(pd.getName().toLowerCase())) {
-                    System.out.println(pd.toString());
                     context.setVariable("category", productService.getProductCategory(pd.getId()));
                     context.setVariable("products", productService.getProductsForCategory(pd.getId()));
                 } else {
@@ -43,10 +45,14 @@ public class ProductController extends HttpServlet {
                     context.setVariable("products", productService.getProductsForCategory(1));
                 }
             }
+        } else if(supplier != null){
+            context.setVariable("products", productService.getSupplierDao(supplier));
+            context.setVariable("currentSupplier", supplier);
         }else {
             context.setVariable("category", productService.getProductCategory(1));
             context.setVariable("products", productService.getProductsForCategory(1));
         }
+        context.setVariable("supplierList", supplierDao.getAll());
 
         context.setVariable("categories", productCategoryDataStore.getAll());
         // // Alternative setting of the template context
