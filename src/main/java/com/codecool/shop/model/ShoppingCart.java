104a9a977ff2286eecbe78901f124e9118ab2f34
tothgeza <toth.geza.0425@gmail.com>
2021-06-08 13:46:20 +0200

    Define ShoppingCart class

--- src/main/java/com/codecool/shop/model/ShoppingCart.java
+++ src/main/java/com/codecool/shop/model/ShoppingCart.java
@@ -4,17 +4,26 @@ import java.util.List;
 
 public class ShoppingCart {
     private List<Product> products;
+    private List<LineItem> lineItems;
 
-
-    public ShoppingCart(){
-
-    }
 
     public List<Product> getProducts() {
         return products;
     }
 
-    public void setProducts(List<Product> products) {
-        this.products = products;
+    public void addProduct(Product product) {
+        this.products.add(product);
+    }
+
+    public List<LineItem> getLineItems() {
+        return lineItems;
+    }
+
+    public void addLineItem(LineItem lineItem) {
+        this.lineItems.add(lineItem);
+    }
+
+    public void removeLineItem(LineItem lineItem){
+        this.lineItems.remove(lineItem);
     }
 }
