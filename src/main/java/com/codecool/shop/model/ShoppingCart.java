5ed12c3b7e40e3388e482921f15b94ca0c41991e
Toth Geza <toth.geza.0425@gmail.com>
2021-06-09 10:38:12 +0200

    Fix LineItem

--- src/main/java/com/codecool/shop/model/ShoppingCart.java
+++ src/main/java/com/codecool/shop/model/ShoppingCart.java
@@ -5,18 +5,19 @@ import java.util.List;
 
 public class ShoppingCart {
     private List<LineItem> lineItems = new ArrayList<>();
-    private LineItem lineItem;
 
     public ShoppingCart(Product product){
         addProduct(product);
     }
     public void addProduct(Product product) {
         if (getLineItemByProduct(product) != null){
-            lineItem = new LineItem(product);
+            LineItem lineItem = getLineItemByProduct(product);
+            this.lineItems.add(lineItem);
         } else {
-            lineItem = getLineItemByProduct(product);
+            LineItem lineItem = new LineItem(product);
+            this.lineItems.add(lineItem);
         }
-        this.lineItems.add(lineItem);
+
     }
 
     public LineItem getLineItemByProduct(Product product){
