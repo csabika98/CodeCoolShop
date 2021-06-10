d7d602059c742d1520132475272792955d4f804d
tothgeza <toth.geza.0425@gmail.com>
2021-06-10 17:34:43 +0200

    Define CheckoutController class

--- src/main/java/com/codecool/shop/controller/CheckoutController.java
+++ src/main/java/com/codecool/shop/controller/CheckoutController.java
@@ -1,6 +1,9 @@
 package com.codecool.shop.controller;
 
 import com.codecool.shop.config.TemplateEngineUtil;
+import com.codecool.shop.model.BillingAddress;
+import com.codecool.shop.model.ShippingAddress;
+import com.codecool.shop.model.User;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
@@ -18,6 +21,29 @@ import java.io.IOException;
             TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
             WebContext context = new WebContext(req, resp, req.getServletContext());
             engine.process("checkout/checkout.html", context, resp.getWriter());
+        }
+        protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+            String firstName = req.getParameter("firstName");
+            String lastName = req.getParameter("lastName");
+            String email = req.getParameter("email");
+            String phone = req.getParameter("phone");
+            String bilCountry = req.getParameter("bilCountry");
+            String bilCity = req.getParameter("bilCity");
+            int bilZip = Integer.parseInt(req.getParameter("bilZip"));
+            String bilStreet = req.getParameter("bilStreet");
+            int bilHouse = Integer.parseInt(req.getParameter("bilHouse"));
+            String shipCountry = req.getParameter("shipCountry");
+            String shipCity = req.getParameter("shipCity");
+            int shipZip = Integer.parseInt(req.getParameter("shipZip"));
+            String shipStreet = req.getParameter("shipStreet");
+            int shipHouse = Integer.parseInt(req.getParameter("shipHouse"));
+            BillingAddress newBilling = new BillingAddress(bilCountry, bilCity, bilZip, bilStreet, bilHouse);
+            ShippingAddress newShipping = new ShippingAddress(shipCountry, shipCity, shipZip, shipStreet, shipHouse);
+            User newUser = new User(firstName, lastName, email, phone, newBilling, newShipping);
+//            System.out.println(newUser.toString());
+
+            resp.sendRedirect(req.getContextPath() + "/payment");
+
         }
     }
 
