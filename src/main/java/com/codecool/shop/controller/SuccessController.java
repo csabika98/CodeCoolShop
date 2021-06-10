1c9ba4ff6243f5e9d4bbe240f93efcc5b9e3440d
tothgeza <toth.geza.0425@gmail.com>
2021-06-10 18:46:23 +0200

    Define SuccessController class

---
+++ src/main/java/com/codecool/shop/controller/SuccessController.java
@@ -0,0 +1,21 @@
+package com.codecool.shop.controller;
+
+import com.codecool.shop.config.TemplateEngineUtil;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = {"/success"})
+public class SuccessController  extends HttpServlet {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
+        WebContext context = new WebContext(req, resp, req.getServletContext());
+        engine.process("payment/success.html", context, resp.getWriter());
+    }
+}
