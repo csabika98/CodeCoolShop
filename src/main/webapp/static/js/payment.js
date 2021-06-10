850b6ece249c36435e9882b9b55e6af6f609c015
Toth Geza <toth.geza.0425@gmail.com>
2021-06-10 13:50:51 +0200

    Payment v.1.1

--- src/main/webapp/static/js/payment.js
+++ src/main/webapp/static/js/payment.js
@@ -1,11 +1,16 @@
 const initPayment = {
     AddPaymentListener: function () {
         const paypalRadio = document.querySelector("#paypal");
+        const creditRadio = document.querySelector("#credit");
+        const paypal = document.querySelector(".paypal")
+        const credit = document.querySelector(".credit-card")
         paypalRadio.addEventListener("change", function ( event){
             if (paypalRadio.checked === true){
-
+                paypal.setAttribute("hidden", true);
+                credit.setAttribute("hidden", false);
             } else {
-
+                paypal.setAttribute("hidden", false);
+                credit.setAttribute("hidden", true);
             }
         })
 
