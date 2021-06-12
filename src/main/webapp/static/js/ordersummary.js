290d91b986b26937739c94f2fa6b0a1ff6360cd7
tothgeza <toth.geza.0425@gmail.com>
2021-06-12 17:07:14 +0200

    Refactor code

--- src/main/webapp/static/js/ordersummary.js
+++ src/main/webapp/static/js/ordersummary.js
@@ -1,4 +1,3 @@
-let totalPrice = 0
 const initCart = {
     fetchData: function () {
         fetch('/api/cart')
@@ -7,36 +6,6 @@ const initCart = {
 
     },
     ShoppingSummary: function (data) {
-        // if (data !== null) {
-        //     for (item of data.lineItems) {
-        //         let row = document.createElement('tr')
-        //         row.classList.add("table-success")
-        //         let col1 = document.createElement('td')
-        //         let col2 = document.createElement('td')
-        //         let col3 = document.createElement('td')
-        //         let delbtn = document.createElement("button")
-        //         delbtn.setAttribute("type","button")
-        //         delbtn.addEventListener("click",function () {
-        //             const cart = document.getElementById("myTable");
-        //             initCart.delRow(delbtn)
-        //             pr.textContent = "";
-        //         })
-        //         col1.innerHTML = item.productName
-        //         col2.innerHTML = item.quantity
-        //         col3.innerHTML = item.price
-        //         row.appendChild(col1)
-        //         row.appendChild(col2)
-        //         row.appendChild(col3)
-        //         totalPrice += item.price
-        //         document.querySelector('tbody').appendChild(row).appendChild(delbtn)
-        //     }
-        //     const pr = document.createElement("h2")
-        //     pr.style.float = "right";
-        //     console.log("why????")
-        //     pr.textContent = "Total price: " + totalPrice.toFixed(2)
-        //     document.querySelector('tbody').appendChild(pr)
-        //     console.log("1")
-        //     }
         let tbody = document.querySelector('.orderBody');
         tbody.innerHTML = "";
         let index = 1;
@@ -80,5 +49,4 @@ const initCart = {
 window.onload = function () {
     initCart.fetchData();
     initCart.ShoppingSummary();
-    // initCart.postData();
 }
